ae54662d84760cff663239ce69f0525529f28292
skydzhugashvili5 <nicholas42tu.rne.r1.1.97@gmail.com>
2024-08-02 09:03:00 -0400

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiNzQ4MmYzNTIwMTU2OTg0MTA3MTBiYjA2NjljMWQzMWYiLCJ0IjoiZmMwMjJiZWQtOTdjZS00NzBkLTgxMzYtYjZjM2FjOWFjOGEzIiwicyI6Ik16QTFObU0wWkRJdE5UYzFNQzAwTkRSaExXRmhaREF0TnpJMllXSTJORFZpWkROaCJ9
